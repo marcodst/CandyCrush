18e71d07df3812f51ce9047c42656ab07b2c4461
marcodst <65162412+marcodst@users.noreply.github.com>
2020-05-14 19:10:20 +0200

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -2,11 +2,23 @@ public class Candy {
     private String emoji;
     private int value;
     private String name;
-    public Candy(String name, String emoji, int value) {
+    public enum Mark {X, O};
+    private Mark mark;
+    public Candy(String name, String emoji, int value, Mark mark) {
         this.name = name;
         this.emoji = emoji;
         this.value = value;
+        this.mark = mark;
     }
+
+    public void setMark(Mark mark) {
+        this.mark = mark;
+    }
+
+    public Mark getMark() {
+        return mark;
+    }
+
     @Override
     public String toString() {
         return this.emoji;
