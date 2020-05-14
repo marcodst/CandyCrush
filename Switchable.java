fcbfa750b78b610af1adb02184ff85c317d9069f
marcodst <65162412+marcodst@users.noreply.github.com>
2020-05-14 19:12:29 +0200

    Update Switchable.java

--- Switchable.java
+++ Switchable.java
@@ -1,8 +1,10 @@
 public class Switchable {
-    private final Object[][] grid;
+    public final Object[][] grid;
+
     public Switchable(final int size1, final int size2) {
         grid = new Object[size1][size2];
     }
+
     public void switchTile(int x1, int y1, int x2, int y2) {
         try {
             if (this.canSwitch(x1, y1, x2, y2)){
@@ -14,6 +16,7 @@ public class Switchable {
             System.out.println(ex);
         }
     }
+
     public boolean canSwitch(final int x1, final int y1, final int x2, final int y2) {
         // if (
         //     (x1 == x2 && (y1 == y2 +1 || y1 == y2 -1)) ||
@@ -26,6 +29,7 @@ public class Switchable {
                 (x1 == x2 && ((y1 == y2 + 1) || (y1 == y2 - 1))) ||
                         (y1 == y2 && ((x1 == x2 + 1) || (x1 == x2 - 1)));
     }
+
     public void setValue(final int x, final int y, final Object value) {
         try {
             this.grid[x][y] = value;
@@ -33,6 +37,7 @@ public class Switchable {
             System.out.println(ex);
         }
     }
+
     public Object getValue(final int x, final int y) {
         try {
             return this.grid[x][y];
@@ -41,10 +46,19 @@ public class Switchable {
             return null;
         }
     }
+
+    public boolean areIntoGrid(int x, int y, int i, int j) {
+        if( ((x < grid.length) && (i < grid.length) && (y < grid[0].length) && (j < grid[0].length))
+                && ((x >= 0) && (i >= 0) && (y >= 0) && (j >= 0)) )
+            return true;
+        return false;
+    }
+
     public String toString() {
         String result = "";
+        System.out.println("\n\t\t3\t2\t1\t0\n");
         for (int x = 0; x< this.grid.length; x++) {
-            result += "[";
+            result += x + "\t" + "[";
             for(int y=this.grid[x].length -1; y>=0; y--) {
                 final String space = (int) this.grid[x][y] < 10 ? " " : "";
                 result += "[" + space + this.grid[x][y] + "]";
